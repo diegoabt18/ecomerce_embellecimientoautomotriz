d0132cdc59c92001243d679d1da6452c144a37ae
diegoabt18 <diegoalbertbt@gmail.com>
2022-03-06 12:16:55 -0500

    Primera configuracion

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    colors:{
+      'color1': '#F4F8F8',
+      'color2': '#AFDBDB',
+      'color3': '#9A4A53',
+      'color4': '#CFA2A5',
+      'color5': '#AE8484',
+    },
+    fontFamily:{
+      ptSans: ['PT Sans', 'sans-serif'],
+      nunito: ['Nunito', 'serif'],
+      poppins: ['Poppins', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
